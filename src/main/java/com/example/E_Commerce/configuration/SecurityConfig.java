422460ac32a5314f37c9e926e573bf1565f366cf
VigneshRaja-A <vigneshraja1618@gmail.com>
2026-07-04 16:13:08 +0530

    refactor: update auth request matchers to use wildcard

--- src/main/java/com/example/E_Commerce/configuration/SecurityConfig.java
+++ src/main/java/com/example/E_Commerce/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/register").permitAll()
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults())
                 .csrf(csrf ->csrf.disable());
