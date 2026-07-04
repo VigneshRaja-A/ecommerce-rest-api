c593b3c2b1a1ca33f0f5336f7bed0c532265e595
VigneshRaja-A <vigneshraja1618@gmail.com>
2026-07-04 16:17:33 +0530

    feat: implement JWT authentication for login endpoint

--- src/main/java/com/example/E_Commerce/controller/AuthController.java
+++ src/main/java/com/example/E_Commerce/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.example.E_Commerce.controller;
 
+import com.example.E_Commerce.dto.LoginDto;
 import com.example.E_Commerce.entity.Users;
 import com.example.E_Commerce.service.UserService;
+import com.example.E_Commerce.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,15 +17,24 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Users user)
     {
         userService.registerNewUser(user);
         return ResponseEntity.ok("User registered successfully!!");
     }
-    @GetMapping("/Login")
-    public ResponseEntity<String> logIn()
+    @PostMapping("/login")
+    public ResponseEntity<String> logIn(@RequestBody LoginDto loginDto)
     {
-        return ResponseEntity.ok("You have Logged in already");
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getUsername(),loginDto.getPassword())
+            );
+           return ResponseEntity.ok(jwtUtil.generateJwtToken(loginDto.getUsername()));
     }
 }
