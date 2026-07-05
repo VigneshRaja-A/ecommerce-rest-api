14c090013e58871e74297a5fad4a7abddffcb565
VigneshRaja-A <vigneshraja1618@gmail.com>
2026-07-05 14:18:49 +0530

    refactor: replace basic authentication with custom JwtAuthFilter in security config

--- src/main/java/com/example/E_Commerce/configuration/SecurityConfig.java
+++ src/main/java/com/example/E_Commerce/configuration/SecurityConfig.java
@@ -1,4 +1,6 @@
 package com.example.E_Commerce.configuration;
+import com.example.E_Commerce.filters.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,32 +9,26 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(withDefaults())
-                .csrf(csrf ->csrf.disable());
+       http.authorizeHttpRequests(auth ->
+               auth.requestMatchers("api/auth/**").permitAll()
+                       .anyRequest().authenticated())
+                       .csrf(csrf -> csrf.disable());
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
-
 
     @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,PasswordEncoder passwordEncoder)
